a3affe164905d62ae8d227a85e762f7b3ef999fa
Melanie Moore <mcmoore@uindy.edu>
2018-05-22 23:16:21 -0400

    Finished styling button and began to style other elements

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,27 +5,87 @@ import newIcon from './new.png'
 import newHover from './new-hover.png'
 
 const Sidebar = () => {
-return (
-<div className = "Sidebar">
-<div className = "logo">
-<img src = {quill} alt = "Notehearder" />
-</div>
+  return (
+    <div
+      className="Sidebar"
+      style={styles.sidebar}
+    >
+      <div
+        className="logo"
+        style={styles.logo}
+      >
+        <img
+          src={quill}
+          alt="Noteherder"
+          style={styles.logoImg}
+        />
+      </div>
 
 
+      <a href="/notes">
+        <img
+          src={newHover}
+          alt="New note"
+          style={styles.newNote}
+        />
+        <img
+          src={newIcon}
+          alt="New note"
+          style={styles.noteImg}
+        />
+      </a>
 
+      <div className="SignOut"
+       style={styles.signOut}
+      > <button>
+          <i className="fa fa-sign-out"
+          style={styles.signOutButton}
+          ></i>
+        </button>
+      </div>
+    </div>
+  )
+}
 
-<a href = "a">
-<img
-    src={newHover}
-    alt = "New note"
-    />
-    <img
-      src = {newIcon}
+const styles = {
+  sidebar: {
+    width: '6rem',
+    backgroundColor: '#f3f3f3',
+    padding: '0.5rem 0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  logo: {
+    fontFamily: '"Fauna One"',
+    color: '#666',
+    fontSize: '3rem',
+  },
+  logoImg: {
+    width: '3rem',
+    paddingLeft: '0.4rem',
+  },
+  signOut: {
+backgroundColor: '#f3f3f3',
+position: 'relative',
+  top: '400px',
+  left: '0px',
+},
 
-</a>
-</div>
+signOutButton:{
+color: '#008bf8',
+fontSize: '1.5rem',
+},
 
-)
+newNotes: {
+backgroundColor: 'ffffff',
+border: '#8A2BE2',
+},
+
+noteImg: {
+border: '#8A2BE2',
+
+}
 
 }
 
