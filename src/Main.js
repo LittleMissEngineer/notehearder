6dc273c6d0a183fcc41538b6fc5579f9a413e819
Melanie Moore <mcmoore@uindy.edu>
2018-05-23 11:13:27 -0400

    NoteForm.css error is cleared and the page appears with some working features

--- src/Main.js
+++ src/Main.js
@@ -4,23 +4,58 @@ import Sidebar from './Sidebar'
 import NoteList from './NoteList'
 import NoteForm from './NoteForm'
 
-const Main = () => {
-return ( 
-
-
-<div className = "Main" style ={style}>
-    <Sidebar />
-    <NoteList />
-    <NoteForm />
-    </div>
-)
-}
-
-const style = {
+class Main extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        currentNote: {
+          id: null,
+          title: '',
+          body: '',
+        },
+        notes: [
+          {
+            id: 1,
+            title: 'Why I <3 JS',
+            body: 'Because I like code, and JS is code.',
+          },
+          {
+            id: 2,
+            title: 'Thoughts on breakfast',
+            body: 'I love it!',
+          },
+          {
+            id: 3,
+            title: 'Watching the first episode of Black Mirror with your parents',
+            body: 'Don\'t.',
+          },
+        ]
+      }
+    }
+  
+    setCurrentNote = (note) => {
+      this.setState({ currentNote: note })
+    }
+  
+    render() {
+      return (
+        <div className="Main" style={style}>
+          <Sidebar />
+          <NoteList
+            notes={this.state.notes}
+            setCurrentNote={this.setCurrentNote}
+          />
+          <NoteForm currentNote={this.state.currentNote} />
+        </div>
+      )
+    }
+  }
+  
+  const style = {
     display: 'flex',
     height: '100vh',
     alignItems: 'stretch',
-}
-
+  }
+  
 
 export default Main 
